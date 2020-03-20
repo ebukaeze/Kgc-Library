023fc2e18e1e7f3831229f0262eee1daaa6ff30e
kingbaddo <wizzyblaq94@gmail.com>
2020-03-20 08:29:30 +0100

    intitial setup of the project

---
+++ server.js
@@ -0,0 +1,47 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+  }
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const indexPage = require('./routes/index');
+
+const mongoose = require('mongoose');
+const app = express();
+
+//connecting to our MongoDB DataBase
+const uri = process.env.DATABASE_URL;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true });
+    
+    const db = mongoose.connection;
+    db.once('open', () =>{
+        console.log('MongoDB connnection established successfully');
+    });
+    db.on('error', (err) => {
+        console.log('Mongodb connection error:' + err);
+    })
+   db.on('Disconnected', () =>{
+       console.log('MongoDB is disconnected');
+   });
+
+//setting the templating engine and layouts
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
+
+
+//MiddleWares for our Application
+app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexPage);
+
+const port = process.env.Port || 5000;
+
+app.listen(port, () => {
+    console.log(`The app is running on port ${port}` );
+});
+
